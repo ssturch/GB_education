84c7493dfb396ac58f54e51b3350e518a6b276bf
Sergey Turchenkov <ssturch@gmail.com>
2022-08-04 14:21:39 +0300

    исправлен код 2-го примера

--- C#/lection_1/example_2/Program.cs
+++ C#/lection_1/example_2/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите Ваше имя: ");
+string userName = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(userName);
+
