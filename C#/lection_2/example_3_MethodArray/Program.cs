be4acedf19444d78308eb8270d3e28b2c100645b
Sergey Turchenkov <ssturch@gmail.com>
2022-08-08 14:26:09 +0300

    Добавлены упражнения 2-ой лекции

---
+++ C#/lection_2/example_3_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {3,12,51,1,5,5,4,12,4,5};
+
+int n = array.Length;
+int find = 5;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
