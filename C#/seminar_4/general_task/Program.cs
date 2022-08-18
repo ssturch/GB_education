39092e551ba2c6fe74e1fde027ccc6aecf2952d9
Sergey Turchenkov <ssturch@gmail.com>
2022-08-18 20:28:05 +0300

    Решения задач 3-го семинара

--- C#/seminar_4/general_task/Program.cs
+++ C#/seminar_4/general_task/Program.cs
@@ -1,5 +1,40 @@
-﻿void task25func ()
-{   
+﻿//------------------------------------------------------------------------------------------------------------------------------------
+//Стартовый код
+Console.WriteLine("************************************************************");
+Console.WriteLine("Данная программа содержит решения 4-х заданий 3-го семинара.");
+Console.WriteLine("'1' - решение задачи 25");
+Console.WriteLine("'2' - решение задачи 27");
+Console.WriteLine("'3' - решение задачи 29");
+Console.WriteLine("'4' - решение задачи *");
+Console.WriteLine("Введите символ (от 1 до 4), соответствующий задаче:");
+string numTask = Console.ReadLine();
+
+switch (numTask)
+{
+    case "1":
+    task25func ();
+    break;
+
+    case "2":
+    task27func ();
+    break;
+
+    case "3":
+    task29func ();
+    break;
+
+    case "4":
+    taskExtra ();
+    break;
+
+    default:
+    Console.WriteLine("Такой задачи в списке нет!");
+    break;
+}
+//------------------------------------------------------------------------------------------------------------------------------------
+//код задачи №25
+void task25func ()
+{   Console.WriteLine("**************************************");
     Console.WriteLine("Программа возводит число A в степень B");
     Console.WriteLine("**************************************");
     Console.Write("Введите число A: ");
@@ -12,12 +47,24 @@
     {
         int result = funcPow(num, pow);
         if (num != 0 && pow != 0 && result == 0) Console.Write($"Ошибка: Результат вычисления {num}^{pow} переполняет переменную int");
-        else Console.Write($"Результат: {num}^{pow} = {result}");
+        else Console.WriteLine($"Результат: {num}^{pow} = {result}");
     }
 }
 
+int funcPow (int num, int pow)
+{
+    int result = 1;
+    for (int i = 1; i <= pow; i++)
+    {
+        result *= num;
+    }
+    return result;
+}
+//------------------------------------------------------------------------------------------------------------------------------------
+//код задачи №27
 void task27func ()
 {   
+    Console.WriteLine("***********************************************");
     Console.WriteLine("Программа суммирует все цифры в введенном числе");
     Console.WriteLine("***********************************************");
     Console.Write("Введите число: ");
@@ -26,8 +73,26 @@ void task27func ()
     Console.Write($"Сумма всех цифр в числе {num} = {funcSummDigits(num)}");
 }
 
+int funcSummDigits (int num)
+{
+    int result = 0;
+    int lastDigit = 0;
+    int summ = 0;
+    do
+    {      
+        lastDigit += lastDigit;
+        lastDigit = num % 10;
+        num /= 10;
+        summ += lastDigit;
+    }
+    while (num != 0);
+    return summ;
+}
+//------------------------------------------------------------------------------------------------------------------------------------
+//код задачи №27
 void task29func ()
-{  
+{   
+    Console.WriteLine("**************************************************************");
     Console.WriteLine("Программа создает массив из N чисел, случайно взятых из [A, B)");
     Console.WriteLine("**************************************************************");
     Console.Write("Введите количество чисел N: ");
@@ -46,45 +111,6 @@ void task29func ()
     Console.Write($"Результат: {output}"); 
 }
 
-// void taskExtra ()
-// {
-//     Console.WriteLine("Функция, рисующая ёлочку согласно её высоте");
-//     Console.WriteLine("*******************************************");
-//     Console.Write("Введите высоту ёлочки: ");
-//     int height = int.Parse(Console.ReadLine());
-//     funcFirTree(height);
-
-// }
-
-
-
-
-int funcPow (int num, int pow)
-{
-    int result = 1;
-    for (int i = 1; i <= pow; i++)
-    {
-        result *= num;
-    }
-    return result;
-}
-
-int funcSummDigits (int num)
-{
-    int result = 0;
-    int lastDigit = 0;
-    int summ = 0;
-    do
-    {      
-        lastDigit += lastDigit;
-        lastDigit = num % 10;
-        num /= 10;
-        summ += lastDigit;
-    }
-    while (num != 0);
-    return summ;
-}
-
 int[] funcArrFromAtoB(int qty, int valueA, int valueB)
 {
     int index = 0;
@@ -97,25 +123,45 @@ int[] funcArrFromAtoB(int qty, int valueA, int valueB)
     return array;
 }
 
-// void funcFirTree (int height)
-// {   
-//     Console.Clear();
-//     int x = 10; // точка старта (верхушка ёлки)
-//     int k = 3; // минимальная высота секции елки
-//     int f = 2; // множитель размеров секции елки
-//     for (int i = 1; i < height - 1; i++)
-//     {
-//         for (int j = 0; j < i * k; j++)
-//         {
-//             for (int n = 0; n < )
-//         Console.SetCursorPosition(x + j,j);
-//         Console.WriteLine("+");
-//         }
-//     }
-// }
+//------------------------------------------------------------------------------------------------------------------------------------
+//код задачи со звездочкой
 
-task25func ();
-//task27func ();
-//task29func ();
+void taskExtra ()
+{
+    Console.WriteLine("*******************************************");
+    Console.WriteLine("Функция, рисующая ёлочку согласно её высоте");
+    Console.WriteLine("*******************************************");
+    Console.Write("Введите высоту ёлочки: ");
+    int height = int.Parse(Console.ReadLine());
+    funcFirTree(height);
 
-//taskExtra ();
+}
+
+void funcFirTree (int maxHeight)
+{   
+    Console.Clear();
+    int startPoint = 100; // точка старта (верхушка ёлки)
+    int minHeight = 3; // минимальная высота секции елки
+    int multiplRatio = 3; // множитель размеров секции елки
+    int minChrPlace = 0; // положение строки начала новой секции
+    int x = 0;
+    int y = 0; 
+
+    for (int i = 0; i < maxHeight; i++) // цикл построения секций елки
+    {
+        if (i != 0) minChrPlace = minChrPlace + minHeight; // смещение строки начала новой секции, если строится не первая секция
+        if (i != 0) minHeight = minHeight + multiplRatio; // изменение высоты секции, если строится не первая секция
+        int m = 1;                                       // счетчик для получения нечетного количества символов
+        for (int j = 0; j < minHeight; j++) // цикл построения строк секций (построчно)
+        {         
+            for (int n = 0; n < m; n++) // цикл отрисовки символов в строке секции
+            {
+                x = startPoint + n - j; // положение указателя с учетом стартовой точки и смещения по столбцу
+                y = j + minChrPlace; // положение указателя с учетом стартовой точки и смещения по строке
+                Console.SetCursorPosition(x,y); 
+                Console.Write("*");
+            }
+            m += 2; // инкремент счетчика для получения нечетного количества символов      
+        }
+    }
+}
