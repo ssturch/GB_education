dcb94a4fa441d5d0c569181438a21df9c5b3116c
Sergey Turchenkov <ssturch@gmail.com>
2022-10-04 13:37:33 +0300

    add python ex_seminar_4_ex2

--- Python/seminar_4/exercise_2.py
+++ Python/seminar_4/exercise_2.py
@@ -1,6 +1,15 @@
 # Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
 isError = True
 while isError == True:
     userInput = input("Введите натуральное число: ")
-    if userInput.isnumeric() != True and (0.0000000001 <= float(accuracyInput) <= 0.1): isError = False
+    if userInput.isnumeric() == True: isError = False; userInput = int(userInput)
     else: print("Ошибка ввода, повторите ввод!")
+
+result = []
+
+for i in range(1, userInput + 1):
+    if userInput % i == 0: result.append(i)
+
+print(f"Список простых множителей числа {userInput}: {result}")
+
