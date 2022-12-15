050b5f74d4523c658951937425b6142885236ff0
Sergey Turchenkov <ssturch@gmail.com>
2022-12-15 11:48:18 +0300

    add sem2_exercises JAVA

---
+++ JAVA/seminar_2/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Answer task N1 (it's palindrome): " + Exercises.Ex1("Was it a cat I saw"));
+        System.out.println("Answer task N1 (it's not palindrome): " + Exercises.Ex1("Too bad I hid a bad"));
+        System.out.println("Answer task N2 (it's not palindrome): " + Exercises.Ex2("TEST"));
+    }
+}
